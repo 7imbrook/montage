b252c16778507a6a5495c6a633899bf69d646bde
Piper Chester <piperchester@gmail.com>
2013-08-09 22:18:44 -0400

    Add RootViewController, add double tap gesture.

--- Montage/RootViewController.h
+++ Montage/RootViewController.h
@@ -1,13 +1,13 @@
 //
-//  ViewController.h
+//  RootViewController.h
 //  Montage
 //
-//  Created by Michael Timbrook on 8/9/13.
+//  Created by Piper Chester on 8/9/13.
 //  Copyright (c) 2013 Michael Timbrook. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface RootViewController : UIViewController
 
 @end
---
+++ Montage/RootViewController.m
@@ -0,0 +1,27 @@
+//
+//  RootViewController.m
+//  Montage
+//
+//  Created by Piper Chester on 8/9/13.
+//  Copyright (c) 2013 Michael Timbrook. All rights reserved.
+//
+
+#import "RootViewController.h"
+
+@interface RootViewController ()
+
+@end
+
+@implementation RootViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+@end
