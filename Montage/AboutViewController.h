8e52fa5d77d0a5a973dd6a411bfefb2072ccdcc2
Piper Chester <piperchester@gmail.com>
2013-08-09 22:13:36 -0400

    Add about vc, change view color, add segue.

---
+++ Montage/AboutViewController.h
@@ -0,0 +1,13 @@
+//
+//  AboutViewController.h
+//  Montage
+//
+//  Created by Piper Chester on 8/9/13.
+//  Copyright (c) 2013 Michael Timbrook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+
+@end
---
+++ Montage/AboutViewController.m
@@ -0,0 +1,27 @@
+//
+//  AboutViewController.m
+//  Montage
+//
+//  Created by Piper Chester on 8/9/13.
+//  Copyright (c) 2013 Michael Timbrook. All rights reserved.
+//
+
+#import "AboutViewController.h"
+
+@interface AboutViewController ()
+
+@end
+
+@implementation AboutViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+@end
