59f663ae80c4bbad57c6e08b744f17924dfdd38e
7imbrook <timbrook480@gmail.com>
2013-08-10 19:34:19 -0400

    Added ImageViewPickerAlbum class for selecting albums

---
+++ Montage/ImagePickerAlbumViewController.h
@@ -0,0 +1,13 @@
+//
+//  ImagePickerAlbumViewController.h
+//  Montage
+//
+//  Created by Michael Timbrook on 8/10/13.
+//  Copyright (c) 2013 Michael Timbrook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImagePickerAlbumViewController : UIViewController
+
+@end
---
+++ Montage/ImagePickerAlbumViewController.m
@@ -0,0 +1,13 @@
+//
+//  ImagePickerAlbumViewController.m
+//  Montage
+//
+//  Created by Michael Timbrook on 8/10/13.
+//  Copyright (c) 2013 Michael Timbrook. All rights reserved.
+//
+
+#import "ImagePickerAlbumViewController.h"
+
+@implementation ImagePickerAlbumViewController
+
+@end
